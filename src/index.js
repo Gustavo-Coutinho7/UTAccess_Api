69e56fbf7c5bfa08c9071f1c341685f12509bb88
Gustavo-Coutinho7 <gustavocoutinho7@outlook.com>
2020-06-11 11:44:40 -0300

    Port error

--- src/index.js
+++ src/index.js
@@ -8,5 +8,5 @@ app.use(express.urlencoded({extended: false}));
 //routes
 app.use(require('./routes/index'));
 
-app.listen(3000);
-console.log("Listen on port 3000"); 
+app.listen(process.env.PORT);
+console.log("Listen!!!"); 
